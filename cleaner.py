c5c3cd73ad534a44d37296ec2bf574da68ebd79c
Joseph Catrambone <jo.jcat@gmail.com>
2014-08-05 16:05:08 -0400

    Move around some files.

--- cleaner.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-# cleaner.py -- The task which pulls images from the database, then checks to make sure the links are still valud.
-
---
+++ hashtools.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+# hasher.py -- The daemon which sits in the background and monitors for newly added images, then adds the hashes to the database.
