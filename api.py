ad1bf96dee87ca1b42951f5e0c3ab6ff9945ab74
Joseph Catrambone <jo.jcat@gmail.com>
2014-08-05 12:46:20 -0400

    First import.

---
+++ api.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+# api.py -- The main service provider.  Serves up page requests and the initial search page doc.
---
+++ models.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# models.py -- Contain the objects for internal data manipulation.  Rely on database.py for backend implementation.
+
