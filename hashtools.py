5940256698a8ec782aeb7c2e98f4075df83c5b4d
Joseph Catrambone <jo.jcat@gmail.com>
2014-08-06 16:54:03 -0400

    First import of hash tools.

--- hashtools.py
+++ hashtools.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
-# hasher.py -- The daemon which sits in the background and monitors for newly added images, then adds the hashes to the database.
+# hashtools.py -- The daemon which sits in the background and monitors for newly added images, then adds the hashes to the database.
+
+from hashlib import sha512
+from Pillow import PIL
+
+HASH_ALGORITHMS = {'exact':exact}
+
+def exact(image):
+	return sha512(image).digest()
+
+def main():
+	pass
+
+if __name__=="__main__":
+	main()
